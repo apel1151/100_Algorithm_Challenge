da7bc25601e1461f86359e0cca6c28aae9f453ba
Apel Mahmud <apelmahmud1151@gmail.com>
2025-01-25 20:03:38 +0800

    Update Day_3.js

--- Day_3.js
+++ Day_3.js
@@ -1,5 +1,6 @@
-// Question: 
+// Question: Give a rectangular matrix of characters, add a border of asterisks(*) to it. use concat(), unshift(), push()
 
+/*****************Answer*********************/
 
 
 const addBorder = (matrix) => {
